68116fae1780812436fddd6044844947068f8426
iaffs <byiaffs@gmail.com>
2021-04-14 23:14:59 +0200

    initial commit

---
+++ App.tsx
@@ -0,0 +1,67 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { createPortal } from 'react-dom';
+import { StyleSheet, Switch, Text, View } from 'react-native';
+
+const App = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  const green = "#AEC69B";
+  const yellow = "#F3D387";
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>MASKE-INGENIØREN</Text>
+
+      <Switch 
+        style={styles.toggle} 
+        trackColor={{ false: "#ECE8D9", true: "#FFFFFF"}}
+        thumbColor={isEnabled ? green : yellow}
+        ios_backgroundColor="#D8D8D8"
+        onValueChange={toggleSwitch}
+        value={isEnabled}></Switch>
+
+      <Text style={styles.calculator}></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#4D4D4D', 
+    overflow: "hidden",
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 40,
+    padding: 15,
+    alignSelf: "center",
+    color: "#fff"
+  },
+  calculator: {
+    width: 800,
+    height: 1000,
+    backgroundColor: "#F3D387",
+    marginTop: 20,
+    borderRadius: 15,
+    overflow: 'hidden',
+    
+  },
+  toggle: {
+    
+  }
+});
+
+
+function backgroundColor() {
+  throw new Error('Function not implemented.');
+}
+/*
+#AEC69B green
+*/
